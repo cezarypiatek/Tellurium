603620f1b4014fbc0cad8714c611c35fd656a33a
Cezary Piatek <cezarypiatek@live.com>
2016-05-07 20:20:38 +0200

    FIX: gitignore. Add missing file (due to invalid gitignore).

---
+++ Src/MaintainableSelenium/MaintainableSelenium.Web/Services/TestResult/TestCaseCategoriesListViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using MaintainableSelenium.Web.Models.TestCase;
+
+namespace MaintainableSelenium.Web.Services.TestResult
+{
+    public class TestCaseCategoriesListViewModel
+    {
+        public List<TestCaseCategoryListItem> Categories { get; set; }
+    }
+}
