aeb7befde3d8329f4324e32f7c2809c6e85f282e
Cezary Piatek <cezarypiatek@live.com>
2016-05-09 21:54:42 +0200

    Add ability to save screenshots in the file system

---
+++ Src/MaintainableSelenium/MaintainableSelenium.MvcPages/BrowserCamera/FileSystemScreenshotStorage.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing.Imaging;
+using System.IO;
+using MaintainableSelenium.MvcPages.Utils;
+
+namespace MaintainableSelenium.MvcPages.BrowserCamera
+{
+    public class FileSystemScreenshotStorage : IScreenshotStorage
+    {
+        private readonly string screenshotDirectoryPath;
+
+        public FileSystemScreenshotStorage(string screenshotDirectoryPath)
+        {
+            this.screenshotDirectoryPath = screenshotDirectoryPath;
+        }
+
+        public void Persist(byte[] image, ScreenshotIdentity screenshotIdentity)
+        {
+            var screenshotFileName = GetScreenshotFileName(screenshotIdentity);
+            var screenshotPath = Path.Combine(screenshotDirectoryPath, screenshotFileName);
+            image.ToBitmap().Save(screenshotPath, ImageFormat.Jpeg);
+        }
+
+        private static string GetScreenshotFileName(ScreenshotIdentity screenshotIdentity)
+        {
+            return string.Format("{0}_{1}_{2}_{3}.png", screenshotIdentity.ProjectName, screenshotIdentity.BrowserName, screenshotIdentity.Category, screenshotIdentity.ScreenshotName);
+        }
+    }
+}
---
+++ Src/MaintainableSelenium/MaintainableSelenium.MvcPages/BrowserCamera/ScreenshotStorageFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace MaintainableSelenium.MvcPages.BrowserCamera
+{
+    public static class ScreenshotStorageFactory
+    {
+        public static IScreenshotStorage CreateFileSystemStorage(string screenshotDirectoryPath)
+        {
+            return new FileSystemScreenshotStorage(screenshotDirectoryPath);
+        }
+    }
+}
