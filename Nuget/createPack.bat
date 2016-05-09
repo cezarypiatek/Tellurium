4e027da3559295191f7834952a2233e28e06cd03
Cezary Piatek <cezarypiatek@live.com>
2016-05-09 21:13:10 +0200

    Add new definition for nuget packages

--- Nuget/createPack.bat
+++ Nuget/createPack.bat
@@ -4,4 +4,5 @@ if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
 set config="Release"
-nuget pack ..\Src\MaintainableSelenium\MaintainableSelenium.Toolbox\MaintainableSelenium.Toolbox.csproj.nuspec -NoPackageAnalysis -verbosity detailed -o ./ -Version %version% -p Configuration="%config%"
+nuget pack ..\Src\MaintainableSelenium\MaintainableSelenium.MvcPages\MaintainableSelenium.MvcPages.csproj.nuspec -NoPackageAnalysis -verbosity detailed -o ./ -Version %version% -p Configuration="%config%"
+nuget pack ..\Src\MaintainableSelenium\MaintainableSelenium.VisualAssertions\MaintainableSelenium.VisualAssertions.csproj.nuspec -NoPackageAnalysis -verbosity detailed -o ./ -Version %version% -p Configuration="%config%"
