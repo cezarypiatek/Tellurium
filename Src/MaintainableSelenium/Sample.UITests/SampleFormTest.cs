2b0b8ae34f0333722bf49f3fd1c9ed8b5eaf93bf
Cezary Piatek <cezarypiatek@live.com>
2016-12-29 23:38:00 +0100

    Add ability to provide browser adapter configuration through app.config

--- Src/MaintainableSelenium/Sample.UITests/SampleFormTest.cs
+++ Src/MaintainableSelenium/Sample.UITests/SampleFormTest.cs
@@ -77,5 +77,51 @@ namespace MaintainableSelenium.Sample.UITests
                 AssertView.EqualsToPattern(browserAdapter, "Sample2");
             }
         }
+
+        [Test]
+        public void should_be_able_to_run_test_with_configuration_from_file()
+        {
+            //Initialize MvcPages
+            var browserAdapterConfig = BrowserAdapterConfig.FromAppConfig(TestContext.CurrentContext.TestDirectory);
+
+            //Initialize VisualAssertions
+            AssertView.Init(new VisualAssertionsConfig
+            {
+                BrowserName = browserAdapterConfig.BrowserType.ToString(),
+                ProjectName = "Sample Project",
+                ScreenshotCategory = "Sample Form",
+            });
+
+
+            //Prepare infrastructure for test
+            using (var browserAdapter = BrowserAdapter.Create(browserAdapterConfig))
+            {
+                //Test
+                browserAdapter.NavigateTo<TestFormsController>(c => c.Index());
+                AssertView.EqualsToPattern(browserAdapter, "Sample21");
+                
+                browserAdapter.SaveScreenshot("Test");
+                var detinationForm = browserAdapter.GetForm<SampleFormViewModel>(FormsIds.TestFormDst);
+                var sourcenForm = browserAdapter.GetForm<SampleFormViewModel>(FormsIds.TestFormSrc);
+
+                var textInputValue = sourcenForm.GetFieldValue(x=>x.TextInput);
+                detinationForm.SetFieldValue(x => x.TextInput, textInputValue);
+
+                var textAreaValue = sourcenForm.GetFieldValue(x => x.TextAreaInput);
+                detinationForm.SetFieldValue(x => x.TextAreaInput, textAreaValue);
+
+                var passwordValue = sourcenForm.GetFieldValue(x => x.PasswordInput);
+                detinationForm.SetFieldValue(x => x.PasswordInput, passwordValue);
+
+
+                var checkboxValue = sourcenForm.GetFieldValue(x=>x.CheckboxInput);
+                detinationForm.SetFieldValue(x => x.CheckboxInput, checkboxValue);
+
+                var selectListValue = sourcenForm.GetFieldValue(x=>x.SelectListValue);
+                detinationForm.SetFieldValue(x=>x.SelectListValue, selectListValue);
+
+                AssertView.EqualsToPattern(browserAdapter, "Sample22");
+            }
+        }
     }
 }
