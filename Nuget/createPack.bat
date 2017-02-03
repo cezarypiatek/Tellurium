ca46118e3fc6f946223895e3eb78bf1fdb53c0a3
Cezary Piatek <cezarypiatek@live.com>
2017-02-03 22:24:16 +0100

    Remove unnecessary directory

--- Nuget/createPack.bat
+++ Nuget/createPack.bat
@@ -4,5 +4,5 @@ if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
 set config="Release"
-nuget pack ..\Src\MaintainableSelenium\MvcPages\MvcPages.csproj.nuspec -NoPackageAnalysis -verbosity detailed -o ./ -Version %version% -p Configuration="%config%"
-nuget pack ..\Src\MaintainableSelenium\VisualAssertions\VisualAssertions.csproj.nuspec -NoPackageAnalysis -verbosity detailed -o ./ -Version %version% -p Configuration="%config%"
+nuget pack ..\Src\MvcPages\MvcPages.csproj.nuspec -NoPackageAnalysis -verbosity detailed -o ./ -Version %version% -p Configuration="%config%"
+nuget pack ..\Src\VisualAssertions\VisualAssertions.csproj.nuspec -NoPackageAnalysis -verbosity detailed -o ./ -Version %version% -p Configuration="%config%"
