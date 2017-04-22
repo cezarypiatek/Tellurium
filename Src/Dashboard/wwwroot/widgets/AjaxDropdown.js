3541c5a6f39f860b59b5897af4518aa7d855db9a
Cezary Piatek <cezarypiatek@live.com>
2017-04-22 15:17:17 +0200

    Prefetch images

--- Src/Dashboard/wwwroot/widgets/AjaxDropdown.js
+++ Src/Dashboard/wwwroot/widgets/AjaxDropdown.js
@@ -4,7 +4,30 @@
         var that = this;
         this.element.on("change", function () {
             var url = $(this).val();
-            $("#" + that.options.target).load(url);
+            $("#" + that.options.target).loadWithImages(url);
         });
     }
 });
+
+$.fn.loadWithImages = function(url) {
+    var $that = this;
+    $.get(url).done(function (data) {
+        var totalImg = 0;
+        var loadedImg = 0;
+
+        function onImageLoad() {
+            loadedImg++;
+            if (totalImg == loadedImg) {
+                $that.html(data);
+            }
+        }
+
+        $(data).find("img").each(function() {
+            totalImg++;
+            var img = new Image();
+            img.onload = onImageLoad;
+            img.onerror = onImageLoad;
+            img.src = $(this).attr("src");
+        });
+    });
+};
