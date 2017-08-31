d979bda05251cec51a3381e7d1e2d2c8da830cee
Cezary Piatek <cezarypiatek@live.com>
2017-08-31 20:48:11 +0200

    Fix issue with persisting DateTime

---
+++ Src/VisualAssertions/Screenshots/Utils/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tellurium.VisualAssertions.Screenshots.Utils
+{
+    static class DateTimeExtensions
+    {
+        public static DateTime TrimToMiliseconds(this DateTime dateTime)
+        {
+            return Trim(dateTime, TimeSpan.TicksPerMillisecond);
+        }
+
+        static DateTime Trim(DateTime date, long roundTicks)
+        {
+            return new DateTime(date.Ticks - date.Ticks % roundTicks);
+        }
+    }
+}
