fd1192bdcca7400e2f1f0923f9fbc833a1377d54
Cezary Piatek <cezarypiatek@live.com>
2017-11-12 19:46:10 +0100

    Fix extracting exception type

--- Src/MvcPages/Utils/ExceptionHelper.cs
+++ Src/MvcPages/Utils/ExceptionHelper.cs
@@ -12,7 +12,7 @@ namespace Tellurium.MvcPages.Utils
             }
             
             var innerExceptionMessage = exception.InnerException.GetFullExceptionMessage();
-            return $"{innerExceptionMessage}\r\n{typeof(Exception).FullName}: {exception.Message}\r\n{exception.StackTrace}".Trim();
+            return $"{innerExceptionMessage}\r\n{exception.GetType().FullName}: {exception.Message}\r\n{exception.StackTrace}".Trim();
         }
 
         public static T SwallowException<T>(Func<T> func, T defaultValue)
