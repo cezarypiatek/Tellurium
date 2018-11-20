4f7a8858415b4730088a9b0d6a67810aa5e8949c
Cezary Piatek <cezarypiatek@live.com>
2018-11-20 19:25:20 +0100

    Extract page reload detection logic to external component

---
+++ Src/MvcPages/SeleniumUtils/DefaultPageReloadDetector.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium.Remote;
+
+namespace Tellurium.MvcPages.SeleniumUtils
+{
+    internal class DefaultPageReloadDetector : IPageReloadDetector
+    {
+        public void RememberPage(RemoteWebDriver driver)
+        {
+            MarkPageAsVisited(driver);
+        }
+
+        public bool WasReloaded(RemoteWebDriver driver)
+        {
+            return IsNewlyLoadedPage(driver);
+        }
+
+
+        private bool IsNewlyLoadedPage(RemoteWebDriver driver)
+        {
+            return driver.IsPageLoaded() && HasVisitedPageMark(driver) == false;
+        }
+
+        private bool HasVisitedPageMark(RemoteWebDriver driver)
+        {
+            return (bool)driver.ExecuteScript("return window.__selenium_visited__ === true;");
+        }
+
+        private void MarkPageAsVisited(RemoteWebDriver driver)
+        {
+            driver.ExecuteScript("window.__selenium_visited__ = true;");
+        }
+    }
+}
