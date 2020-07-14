45b4b65fc39f75dab10cff55fd8dae7ddceaa90c
Łukasz Pękala <lupe@ailleron.com>
2020-07-14 15:20:50 +0200

    Added PixelByPixelComparisonStrategy

--- Src/VisualAssertions/Screenshots/VisualAssertionsConfig.cs
+++ Src/VisualAssertions/Screenshots/VisualAssertionsConfig.cs
@@ -9,5 +9,23 @@ namespace Tellurium.VisualAssertions.Screenshots
         public string ScreenshotCategory { get; set; }
         public Action<string> TestOutputWriter { get; set; }
         public bool ProcessScreenshotsAsynchronously { get; set; }
+
+        public ComparisonStrategy ComparisonStrategy { get; set; }
+
+        /// <summary>
+        /// Only applicable if ComparisonStrategy is PixelByPixel
+        /// </summary>
+        public int PixelTolerance { get; set; }
+
+        /// <summary>
+        /// Only applicable if ComparisonStrategy is PixelByPixel
+        /// </summary>
+        public int PixelColorTolerance { get; set; }
+    }
+
+    public enum ComparisonStrategy
+    {
+        Hash = 0,
+        PixelByPixel = 1
     }
 }
