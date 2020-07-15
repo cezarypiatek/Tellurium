2fedc0ae68b982c4df9f78af0a5015351ef7084e
Łukasz Pękala <lupe@ailleron.com>
2020-07-15 18:01:33 +0200

    Added result messages + improved, much faster comparison strategy

--- Src/VisualAssertions/Screenshots/Service/ComparisonStrategies/PixelByPixelComparisonStrategy.cs
+++ Src/VisualAssertions/Screenshots/Service/ComparisonStrategies/PixelByPixelComparisonStrategy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Globalization;
 using Tellurium.VisualAssertions.Screenshots.Domain;
 
@@ -21,43 +22,104 @@ namespace Tellurium.VisualAssertions.Screenshots.Service.ComparisonStrategies
         {
             var pattern = browserPattern.PatternScreenshot.Image;
             var blindRegions = browserPattern.GetAllBlindRegions();
-            var numberOfUnmatchedPixels = CountUnmatchedPixels(pattern, screenshot, blindRegions);
+            var patternBitmap = ImageHelpers.ApplyBlindRegions(pattern, blindRegions);
+            var screenshotBitmap = ImageHelpers.ApplyBlindRegions(screenshot, blindRegions);
 
-            const int numberOfChannels = 4;
-            var percentOfUnmatchedPixels = (double) numberOfUnmatchedPixels / pattern.Length / numberOfChannels;
-            var areMatched = numberOfUnmatchedPixels < ComparisonParameters.PixelToleranceCount && percentOfUnmatchedPixels < ComparisonParameters.MaxPercentOfUnmatchedPixels;
+            if (!AreBitmapsOfEqualSize(patternBitmap, screenshotBitmap))
+            {
+                resultMessage = "\r\n+---Different sizes of bitmaps" +
+                                $"\r\n+---Pattern size: {patternBitmap.Size}" +
+                                $"\r\n+---Screenshot size: {screenshotBitmap.Size}";
 
-            resultMessage = $"\r\n+---Pattern size: {pattern.Length} bytes" +
-                            $"\r\n+---Screenshot size: {screenshot.Length} bytes" +
-                            $"\r\n+---Unmatched pixels: {numberOfUnmatchedPixels}/{pattern.Length / numberOfChannels} {percentOfUnmatchedPixels.ToString("0.######", CultureInfo.InvariantCulture)}%";
+                return false;
+            }
+
+            var numberOfUnmatchedPixels = CountUnmatchedPixels(patternBitmap, screenshotBitmap);
+
+            var percentOfUnmatchedPixels = (double)numberOfUnmatchedPixels / pattern.Length * 100;
+            bool areMatched = numberOfUnmatchedPixels <= ComparisonParameters.PixelToleranceCount && percentOfUnmatchedPixels <= ComparisonParameters.MaxPercentOfUnmatchedPixels;
+
+            resultMessage = $"\r\n+---Unmatched pixels: {numberOfUnmatchedPixels}px/{patternBitmap.Height * patternBitmap.Width}px {percentOfUnmatchedPixels.ToString("0.######", CultureInfo.InvariantCulture)}% | (tolerance: {ComparisonParameters.PixelToleranceCount}px, {ComparisonParameters.MaxPercentOfUnmatchedPixels.ToString("0.######", CultureInfo.InvariantCulture)}%)";
 
             return areMatched;
         }
 
-        private int CountUnmatchedPixels(byte[] pattern, byte[] screenshot, IReadOnlyList<BlindRegion> blindRegions)
+        private static bool AreBitmapsOfEqualSize(Bitmap patternBitmap, Bitmap screenshotBitmap)
+        {
+            return patternBitmap.Height == screenshotBitmap.Height && patternBitmap.Width == screenshotBitmap.Width;
+        }
+
+        private int CountUnmatchedPixelsSlow(Bitmap pattern, Bitmap screenshot)
         {
             var numberOfUnmatchedPixels = 0;
-            var imageA = ImageHelpers.ApplyBlindRegions(pattern, blindRegions);
-            var imageB = ImageHelpers.ApplyBlindRegions(screenshot, blindRegions);
 
-
-            for (int k = 0; k + 4 < imageB.Length; k += 4)
+            for (int x = 0; x < pattern.Width; x++)
             {
-                var alpha = k;
-                var red = k + 1;
-                var green = k + 2;
-                var blue = k + 3;
-
-                if (imageA[alpha] != imageB[alpha] ||
-                    imageA[red] != imageB[red] ||
-                    imageA[green] != imageB[green] ||
-                    imageA[blue] != imageB[blue])
+                for (int y = 0; y < pattern.Height; y++)
                 {
-                    numberOfUnmatchedPixels++;
+                    if (pattern.GetPixel(x, y) != screenshot.GetPixel(x, y))
+                    {
+                        numberOfUnmatchedPixels++;
+                    }
                 }
             }
 
             return numberOfUnmatchedPixels;
+        }
+
+        /// <summary>
+        /// This method assumes that the bitmaps are of equal size in terms of pixels
+        /// </summary>
+        /// <param name="pattern"></param>
+        /// <param name="screenshot"></param>
+        /// <returns></returns>
+        public int CountUnmatchedPixels(Bitmap pattern, Bitmap screenshot)
+        {
+            if (!AreBitmapsOfEqualSize(pattern, screenshot))
+                throw new ArgumentException("Bitmaps have different sizes");
+
+            var numberOfUnmatchedPixels = 0;
+
+            BitmapData patternBitmapData = pattern.LockBits(new Rectangle(0, 0, pattern.Width, pattern.Height), ImageLockMode.ReadOnly, pattern.PixelFormat);
+            byte bitsPerPixel = GetBitsPerPixel(patternBitmapData.PixelFormat);
+            int patternSize = patternBitmapData.Stride * patternBitmapData.Height;
+            byte[] patternData = new byte[patternSize];
+            System.Runtime.InteropServices.Marshal.Copy(patternBitmapData.Scan0, patternData, 0, patternSize);
+
+
+            BitmapData screenshotBitmapData = screenshot.LockBits(new Rectangle(0, 0, screenshot.Width, screenshot.Height), ImageLockMode.ReadOnly, screenshot.PixelFormat);
+            int screenshotSize = screenshotBitmapData.Stride * screenshotBitmapData.Height;
+            byte[] screenshotData = new byte[screenshotSize];
+            System.Runtime.InteropServices.Marshal.Copy(screenshotBitmapData.Scan0, screenshotData, 0, screenshotSize);
+
+            for (int i = 0; i < patternSize; i += bitsPerPixel / 8)
+            {
+                if (patternData[i] != screenshotData[i])
+                    numberOfUnmatchedPixels++;
+            }
+
+            pattern.UnlockBits(patternBitmapData);
+            screenshot.UnlockBits(screenshotBitmapData);
+
+            return numberOfUnmatchedPixels;
+        }
+
+        private byte GetBitsPerPixel(PixelFormat pixelFormat)
+        {
+            switch (pixelFormat)
+            {
+                case PixelFormat.Format24bppRgb:
+                    return 24;
+                    break;
+                case PixelFormat.Format32bppArgb:
+                case PixelFormat.Format32bppPArgb:
+                case PixelFormat.Format32bppRgb:
+                    return 32;
+                    break;
+                default:
+                    throw new ArgumentException("Only 24 and 32 bit images are supported");
+
+            }
         }
     }
 }
